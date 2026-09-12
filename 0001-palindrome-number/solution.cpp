94e56d8477a949ef01d28928f446a56bfb2fadcb
Rudresh Patel <rudreshpatel504@gmail.com>
2026-09-12 16:12:18 +0530

    feat: add cpp solution for 0001-palindrome-number

---
+++ 0001-palindrome-number/solution.cpp
@@ -0,0 +1,9 @@
+/*
+ * Problem: 1. Palindrome Number
+ * Link: https://leetcode.com/problems/palindrome-number/
+ * Difficulty: Medium
+ * Language: cpp
+ * Runtime: N/A | Memory: N/A
+ */
+
+// Solution for Palindrome Number
