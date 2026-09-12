8046f51c545044ac6a49d84ce484d9f36fd072e1
Rudresh Patel <rudreshpatel504@gmail.com>
2026-09-12 16:43:43 +0530

    feat: add java solution for 0000-two-sum

---
+++ 0000-two-sum/solution.java
@@ -0,0 +1,25 @@
+/*
+ * Problem: . Two Sum
+ * Link: https://leetcode.com/problems/two-sum/
+ * Difficulty: Medium
+ * Language: java
+ * Runtime: nums | Memory: MB
+ */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int req= target - nums[i];
+            if(map.containsKey(req)){
+                return new int[] {map.get(req), i};
+            }
+            map.put(nums[i], i);
+        } 
+        return new int[] {};   
+             
+        
+    }
+}
+
