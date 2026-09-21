ed0959e486ee56f5f6b37b7826c16c9e7df1c50f
Rudresh Patel <rudreshpatel504@gmail.com>
2026-09-21 23:10:13 +0530

    feat: add java solution for 0001-two-sum

---
+++ 0001-two-sum/solution.java
@@ -0,0 +1,21 @@
+/*
+ * Problem: 1. Two Sum
+ * Link: https://leetcode.com/problems/two-sum/
+ * Difficulty: Medium
+ * Language: java
+ * Runtime: nums | Memory: N/A
+ */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       for (int i=0; i<nums.length; i++){
+         for(int j=i+1; j<nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                return new int[]{i,j};
+            }
+         }
+       } 
+    }
+       return new int[]{};
+}
+
